bf4a0c30ed976d28516c59c900254ee680b145a5
Khalid <reflectonthink@yeah.net>
2022-07-17 11:41:45 +0800

    add comment

--- src/com/asthetik/ch2/HelloDate.java
+++ src/com/asthetik/ch2/HelloDate.java
@@ -2,11 +2,16 @@ package com.asthetik.ch2;
 
 import java.util.Date;
 
+/** The first Thinking in Java example program.
+ * Displays a string and today's date.
+ */
 public class HelloDate {
 
+    /** Entry point to class &amp; application.
+     * @param args array of string arguments
+     */
     public static void main(String[] args) {
-
-        System.out.println("Hello, it's");
+        System.out.println("Hello, it's: ");
         System.out.println(new Date());
     }
 }
---
+++ src/com/asthetik/ch2/ShowProperties.java
@@ -0,0 +1,9 @@
+package com.asthetik.ch2;
+
+public class ShowProperties {
+
+    public static void main(String[] args) {
+
+        System.getProperties().list(System.out);
+    }
+}
